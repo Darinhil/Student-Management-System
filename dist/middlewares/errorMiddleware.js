49675cef1cc622219065ccc77f007b1fdb779add
phorn-ya <phornya26@gmail.com>
2026-05-22 14:56:09 +0700

    remove dist from git

--- dist/middlewares/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-import {} from 'express';
-import logger from '../utils/logger.js';
-export const errorMiddleware = (err, req, res, next) => {
-    logger.error(err.message || 'Internal Server Error');
-    res.status(err.status || 500).json({
-        success: false,
-        message: err.message || 'Internal Server Error',
-    });
-};
--- dist/utils/logger.js
+++
@@ -1,7 +0,0 @@
-const logger = {
-    info: (msg, ...args) => console.log(`[INFO] ${msg}`, ...args),
-    warn: (msg, ...args) => console.warn(`[WARN] ${msg}`, ...args),
-    error: (msg, ...args) => console.error(`[ERROR] ${msg}`, ...args),
-    debug: (msg, ...args) => console.debug(`[DEBUG] ${msg}`, ...args),
-};
-export default logger;
